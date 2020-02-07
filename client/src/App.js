6d093144c925d4a7f4440f6b686e9ec57143ef29
Dylan Thomas <thomasdylan76@gmail.com>
2020-02-07 12:00:57 -0700

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Stock Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
