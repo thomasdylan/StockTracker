48d85282d3959abaff6560a1d36e709c9b3b9f92
Dylan Thomas <thomasdylan76@gmail.com>
2020-03-03 19:06:16 -0700

    Correctly updates when symbol added or removed.

--- client/src/components/MyStocks.js
+++ client/src/components/MyStocks.js
@@ -11,15 +11,17 @@ const MyStocks = () => {
     const [symbolSearch, setSymbolSearch] = useState();
     const [searchResults, setSearchResults] = useState([]);
     const [userSymbols, setUserSymbols] = useState([]);
+    const [isUpdating, setUpdating] = useState(false);
     const { user } = useAuth0();
 
     useEffect(() => {
+        setUpdating(false);
         API.getStock(user.sub)
             .then(res => {
                 setUserSymbols(res.data);
             });
         console.log(userSymbols);
-    }, [getSymbols, removeSymbols]);
+    }, [isUpdating]);
 
     const FormSubmit = (e) => {
         e.preventDefault();
@@ -31,7 +33,9 @@ const MyStocks = () => {
     }
 
     const getSymbols = (name, symbol) => {
-        if(!userSymbols.includes(symbol)) {
+        setUpdating(true);
+        if(userSymbols.includes(symbol) === false) {
+            console.log(userSymbols,"   ", !userSymbols.includes(symbol), " In the if statement")
             setUserSymbols(userSymbols => [...userSymbols, symbol]);
             const stockData = {
                 name: name,
@@ -41,10 +45,13 @@ const MyStocks = () => {
             API.saveStock(stockData)
                 .then(() => console.log("Successfully added symbol"))
                 .catch(err => console.log(err));
+        } else {
+            alert("this item already exist");
         }
     }
 
     const removeSymbols = (id) => {
+        setUpdating(true);
         API.deleteStock(id)
             .then(() => console.log("Sucessfully deleted symbol"))
             .catch(err => console.log(err));
@@ -52,12 +59,12 @@ const MyStocks = () => {
 
     return (
         <div className="flex mb-4">
-            <div className="w-1/3 mt-2 ml-8">
+            <div className="w-1/2 mt-2 ml-8">
                 <form className="w-full max-w-lg" onSubmit={FormSubmit}>
                     <div className="flex items-center border-b border-b-2 border-green-500 py-2">
                         <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" 
                                type="text" 
-                               placeholder="Search by Stock Symbol" 
+                               placeholder="Enter a symbol or keyword" 
                                aria-label="Stock symbol"
                                id="search"
                             onChange={e => setSymbolSearch(e.target.value)}/>
@@ -81,7 +88,7 @@ const MyStocks = () => {
                     })}
                 </div>
             </div>
-            <div className="w-1/3 mt-2 ml-8">                
+            <div className="w-1/2 mt-2 ml-8">                
                 {!userSymbols ?
                     (<div className="flex flex-row max-w-lg overflow-hidden border-b border-b-3 border-green-500 py-2">
                         <h1 className="ml-1 mr-3 font-bold text-lg">No symbols added for {user.name}.</h1>
@@ -91,7 +98,7 @@ const MyStocks = () => {
                             <div className="flex flex-row max-w-lg overflow-hidden border-b border-b-3 border-green-500 py-2">
                                 <h1 className="ml-1 mr-3 font-bold text-lg">{stock.symbol}</h1>
                                 <p className="max-w-0.625rem truncate text-gray-700 text-md">{stock.name}</p> 
-                                <button className="flex-none ml-auto mr-2 fill-current h-6 w-6" onClick={() => removeSymbols(stock._id)}><img src={minus} alt="add" /></button>
+                                <button className="flex-none ml-auto mr-2 fill-current h-6 w-6" onClick={() => removeSymbols(stock._id)}><img src={minus} alt="remove" /></button>
                             </div>
                         )
                     }))
